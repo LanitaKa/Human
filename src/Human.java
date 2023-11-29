91d9c61c8c96d5c0eafffe2147b8c5ad2bc625df
LanitaKa <kaporovych@gmail.com>
2023-11-29 17:25:00 +0200

    start

---
+++ src/Human.java
@@ -0,0 +1,23 @@
+public class Human {
+    String name;
+    String surname;
+    int age;
+    boolean isMan;
+    String hobbies;
+    void displayInfo () {
+        System.out.println("Name: " + name);
+        System.out.println("Surname: " + surname);
+        System.out.println("Age: " + age);
+        System.out.println("Is it a man? " + isMan);
+        System.out.println("Hobbies: " + hobbies);
+        System.out.println("Hi, " + name + "!");
+    }
+
+   public Human (String name, String surname, int age, boolean isMan, String hobbies) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.isMan = isMan;
+        this.hobbies = hobbies;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       Human al = new Human("Alex", "Po", 45, true, "Programming");
+       al.displayInfo();
+
+       System.out.println();
+
+       Human sv = new Human("Lanita", "Ka", 43, false, "Cross stitching");
+       sv.displayInfo();
+
+        }
+    }
