7f0bda6d3aa8747ed67accc5d3e71dd1f1f11aee
LanitaKa <kaporovych@gmail.com>
2023-11-29 18:28:32 +0200

    The position constructor has been fixed.

--- src/Human.java
+++ src/Human.java
@@ -4,21 +4,21 @@ public class Human {
     int age;
     boolean isMan;
     String hobbies;
-    void displayInfo () {
+
+    public Human (String name, String surname, int age, boolean isMan, String hobbies) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.isMan = isMan;
+        this.hobbies = hobbies;
+    }
+
+    public void displayInfo () {
         System.out.println("Name: " + name);
         System.out.println("Surname: " + surname);
         System.out.println("Age: " + age);
         System.out.println("Is it a man? " + isMan);
         System.out.println("Hobbies: " + hobbies);
         System.out.println("Hi, " + name + "!");
-    }
-
-   public Human (String name, String surname, int age, boolean isMan, String hobbies) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.isMan = isMan;
-        this.hobbies = hobbies;
-
     }
 }
